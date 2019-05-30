9f35c79352ad1704b41b0efb2853799624e9b830
Claudius Graebner <claudius@claudius-graebner.com>
2019-05-30 14:41:31 +0200

    Add initial package documentation.

---
+++ R/icaeDesign.R
@@ -0,0 +1,14 @@
+#' icaeDesign: A package to produce figures in the ICAE corporate design style.
+#'
+#' The package provides function to create figures in accordance withe the
+#'  corporate design of the Institute for Comprehensive Analysis of the Economy
+#'  (ICAE) at the Johannes Kepler University in Linz.
+#'
+#' @section Functions to update ggplot2 objects
+#'  The functions \code{\link{scale_color_icae}} and
+#'  \code{\link{scale_fill_icae}} return color schemes for the color and fill
+#'  aesthetic in ggplots.
+#'
+#' @docType package
+#' @name icaeDesign
+NULL
